5a1056cc26d0bce546af4cf57cf03ba2969c7184
Jiang Hunk <28851274@cnbjlx24305.corpusers.net>
2017-06-13 10:01:01 +0800

    Add js auth

--- whitefamily/server/src/main/java/com/whitefamily/web/bean/ajax/WeChatJSAuth.java
+++ whitefamily/server/src/main/java/com/whitefamily/web/bean/ajax/WeChatJSAuth.java
@@ -1,12 +1,19 @@
 package com.whitefamily.web.bean.ajax;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 @AjaxMapping(name ="jsauth", uri="/jsauth.ajaxj", param="", uriMapping=true)
 public class WeChatJSAuth extends AjaxDispatcherJson {
@@ -22,14 +29,47 @@ public class WeChatJSAuth extends AjaxDispatcherJson {
 	@Override
 	public JSONObject onPostRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		JSONObject json = new JSONObject();
-		//TODO get auth information from payment server
 		
+		InputStream in = null;
+		CloseableHttpResponse resp = null;
+		JSONObject json = new JSONObject();
+		String appid = null, ts = null, nonce = null, s = null;
+		try {
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			HttpGet httpGet = new HttpGet("http://localhost:8083/js/auth");
+			resp = httpclient.execute(httpGet);
+			HttpEntity entity1 = resp.getEntity();
+			in = entity1.getContent();
+			byte[] buf = new byte[2048];
+			int n = in.read(buf);
+			if (n >= 2048) {
+				//FIXME should continue to read
+			} else {
+				String str = new String(buf, 0, n);
+				JSONObject object = (JSONObject) new JSONTokener(str).nextValue();
+				if (object.getInt("ret") == 0) {
+					appid = object.getString("appid");
+					ts = object.getString("ts");
+					nonce = object.getString("nonce");
+					s = object.getString("s");
+					
+				}
+				
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (resp != null) {
+				resp.close();
+			}
+		}
+		 
 		json.put("ret", 0);
-		json.put("appid", 0);
-		json.put("timestamp", 0);
-		json.put("nonce", 0);
-		json.put("sign", 0);
+		json.put("appid", appid);
+		json.put("timestamp", ts);
+		json.put("nonce", nonce);
+		json.put("sign", s);
 		
 		return json;
 	}
